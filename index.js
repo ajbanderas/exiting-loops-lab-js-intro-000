9f80c85b310eefbe2816701f296a1fc8e59279cb
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-19 11:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function breakOut(array, changeValue, stopValue){
   array.forEach(element, i => {
-    
+    if(element !== stopValue){
+      element = changeValue
+    }else{
+      break;
+    }
   })
 }
