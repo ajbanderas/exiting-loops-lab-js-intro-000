128b40743b979bcf78b72b52aff6e563fc8a87a5
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-19 11:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array, findFn){
   for(let i = 0; i < array.length; i++){
-    findFn(array[i])
+    if(findFn(array[i])){
+      return array[i]
+    } else{
+      return null
+    }
   }
 }
